54323f0eb3826f169b4352f4f30856d087a2471b
mairitikk <mairi.tikk@gmail.com>
2025-07-18 10:41:25 +0200

    css style changes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,7 +5,7 @@ import {searchImg, bagImg} from '../utils'
 const Navbar = () => {
   return (
     <header className='w-full py-5 sm:px-10 px-5 flex justify-between item-center'>
-      <nav>
+      <nav className='flex w-full screen-max-width'>
         <img src={appleImg} alt="Apple" width={14} height={18} />
         
         <div>
