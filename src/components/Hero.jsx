098414a8f993a095a9d1909225a833e7f0d5ce07
mairitikk <mairi.tikk@gmail.com>
2025-07-20 15:40:20 +0200

    hero changes

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,7 +2,12 @@ import React from 'react'
 
 const Hero = () => {
   return (
-    <div>Hero</div>
+    <section className='w-full nav-heigt bg-black relative'>
+      <div className='h-5/6 w-full flex-center flex-col'>
+      <p className=''>iPhone 15 Pro</p>
+      
+      </div>
+    </section>
   )
 }
 
--- src/components/Highlights.jsx
+++ src/components/Highlights.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Highlights = () => {
   return (
-    <div>Highlights</div>
+    <div></div>
   )
 }
 
